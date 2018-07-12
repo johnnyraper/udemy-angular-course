483e4dd608990b16fa3e92a4c8b27a0c33d7857e
Johnny Raper <johnnyraper@googlemail.com>
2018-07-12 17:57:35 +0100

    Further course development

--- 03-angular-complete/recipeapp2018/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 03-angular-complete/recipeapp2018/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1 Lorem', 'Food is where the heart is', 'https://loremflickr.com/600/400/food'),
+    new Recipe('Recipe 2 Ipsum', 'I love my food', 'https://loremflickr.com/600/410/food')
+  ];
 
   constructor() { }
 
